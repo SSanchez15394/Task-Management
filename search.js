97d5f3c025d1f5a54580c4283d4e7db724b2cf0b
SSanchez <ssanchez15394@gmail.com>
2023-06-08 10:01:44 +0200

    buscador-actualizado-tildes

--- search.js
+++ search.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
     $("#search").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#table-search tbody tr").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
+      var value = $(this).val().toLowerCase();
+      $("#table-search tbody tr").filter(function () {
+        var textoFila = $(this).text().toLowerCase();
+        var textoFilaNormalizado = textoFila.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        var valorNormalizado = value.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        $(this).toggle(textoFilaNormalizado.includes(valorNormalizado));
+      });
     });
-});
+  });
+  
